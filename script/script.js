859a9fd88367fd4e8afdc581acfcec1e7a677773
Specify your username <Your email address>
2024-05-20 08:44:00 +0200

    second commit

--- script/script.js
+++ script/script.js
@@ -1,26 +1,49 @@
-document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear').onclick = clearForm;
+document.addEventListener('DOMContentLoaded', function() {
+    const celsiusField = document.getElementById('celsius');
+    console.log('celsiusField: ' + celsiusField)
+    const fahrenheitField = document.getElementById('fahrenheit');
+    console.log('fahrenheitField: ' + fahrenheitField) 
 
-function tempConvert() {
+    console.log('DOMContentLoaded\n')
+    calculate = document.getElementById('calculate1');
+ 
+    console.log('calculate1\n')
+    calculate.addEventListener('click', function() {
+
+
+        console.log('fahrenheitField: ' + fahrenheitField) 
+
+        console.log('celsiusField.value: ' + celsiusField.value + '\n')        
+        console.log('fahrenheitField.value: ' + fahrenheitField.value + '\n')
+
+        console.log('LEN celsiusField.value: ' + (celsiusField.value).length + '\n')
+        console.log('LEN fahrenheitField.value: ' + (fahrenheitField.value).length + '\n')                
+
+        if ((celsiusField.value).length > 0) 
+            fahrenheitField.value = celciusToFahrenheit(celsiusField.value)
+        else if ((fahrenheitField.value).length > 0) 
+            celsiusField.value = fahrenheitToCelcius(fahrenheitField.value)
+
+
+        
+    });
     
-    var fahrenheit = document.getElementById("fahrenheit").value;
-    var celsius = document.getElementById("celsius").value;
-  
-  if (fahrenheit != '') {
-        celsius = (parseFloat(fahrenheit) - 32) / 1.8;
-    } else {
-        fahrenheit = (parseFloat(celsius) * 1.8) + 32;
-    }
-  
+});
 
+function celciusToFahrenheit(celsius) {
+        let cel = parseFloat((parseFloat(celsius)).toFixed(2));
+        let temp = (cel * 1.8) + 32; 
 
-    document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(2);
-    document.getElementById('celsius').value = parseFloat(celsius).toFixed(2);
+        let result = parseFloat((parseFloat(temp)).toFixed(2));
+        return result;
+    
 }
 
+function fahrenheitToCelcius(fahrenheit) {
+    let fah = parseFloat((parseFloat(fahrenheit)).toFixed(2));
+    let temp = (fah - 32) * 5 / 9;
+    let result = parseFloat((parseFloat(temp)).toFixed(2));
+    return result;
 
-function clearForm() {
-    document.getElementById('fahrenheit').value = '';
-    document.getElementById('celsius').value = '';
-} 
+}
 
